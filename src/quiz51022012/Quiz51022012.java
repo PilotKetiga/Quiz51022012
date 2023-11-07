b436fcd849dbadbe8a39104184b9084ba56793a1
PilotKetiga <150094782+PilotKetiga@users.noreply.github.com>
2023-11-07 11:08:58 +0700

    first

---
+++ src/quiz51022012/Quiz51022012.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz51022012;
+
+
+/**
+ *
+ * @author Asus
+ */
+public class Quiz51022012 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        new Menu();
+    }
+}
+
+
